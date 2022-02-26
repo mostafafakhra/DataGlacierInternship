fda62472925eacff8007d0cf560ac3c2cc3c0eb7
Mostafa Fakhra <97100585+mostafafakhra@users.noreply.github.com>
2022-02-26 20:24:32 +0400

    Update add.py

--- VC-main/scripts/add.py
+++ VC-main/scripts/add.py
@@ -21,8 +21,8 @@ def write_json(data,filename = '../response.json'):
 
 
 def call_sport():
-    name = input("Please add your name: ")
-    sport = input("Please add your favourite sports name: ")
+    name = input("Please add your name: mostafa ")
+    sport = input("Please add your favourite sports name: Gym ")
     if (sport == ""):
         sport = 'Cricket'
     if (name):
