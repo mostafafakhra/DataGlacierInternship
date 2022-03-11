b9eebfd2bc07ca3302f15bc915f918cccfc2b2b6
Mostafa Fakhra <97100585+mostafafakhra@users.noreply.github.com>
2022-03-11 13:35:11 +0400

    Delete w5 directory

--- w5/app/utils/helper_functions.py
+++
@@ -1,7 +0,0 @@
-
-
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
-def allowed_file(filename):
-    # xx.png
-    out = ("." in filename) and (filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS)
-    return out
